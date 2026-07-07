d6e7859220f6934d0f0b108fdd46f2d501e3afc9
Rohit <rohit6thakur9@gmail.com>
2026-07-07 15:21:21 +0530

    lerned two more functions and applied them in our codes such as parsers and prompttemplate

--- dynamicprompt.py
+++ dynamicprompt.py
@@ -2,7 +2,8 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import os
 import streamlit as  st
-
+from langchain_core.output_parsers import StrOutputParser 
+from langchain_core.prompts import PromptTemplate
 st.header("Dynamic Colleger Researcher")
 load_dotenv()
 model=ChatGoogleGenerativeAI(
@@ -13,9 +14,16 @@ option=st.selectbox(
     "Select which university you want to know mroe about",
     ("LPU", "CU", "Amity", "CIPLA")
 )
+
+parser=StrOutputParser()
 if st.button("Search"):
-    combinedinput=f"You are a college researcher and now you have to generate 5 point on the college {option} in a aggressive tone" 
+    prompt_template=PromptTemplate(
+        template="You are a college researcher and now you have to generate 5 point on the college {option} in a aggressive tone",
+        input_variables= ["option"],
+    )
+    # combinedinput=f"You are a college researcher and now you have to generate 5 point on the college {option} in a aggressive tone" 
+    formatted_prompt=prompt_template.format(option=option)
+    response=model.invoke(formatted_prompt)
 
-    result=model.invoke(combinedinput)
-
-    st.write(result.text)
+    result=parser.invoke(response)
+    st.write(result)
